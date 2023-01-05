a51923003fcf579555694708cf96de4ac6be051e
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2023-01-05 16:47:08 +0530

    initial commit

--- basicExpress/index.js
+++ basicExpress/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 //step 1: connect to mongoDB
 mongoose.set("strictQuery", false);
+
 const connect = () => {
     return mongoose.connect("mongodb+srv://mallikarjuna27:Ajju_2748@cluster0.orelrvq.mongodb.net/project?retryWrites=true&w=majority")
 }
