3fa42ad524eb512f12a0c59c6d17fa8c76f697dd
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2023-01-06 00:00:12 +0530

    models

--- Mongo/src/controllers/post.controller.js
+++ Mongo/src/controllers/post.controller.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+const Post = require("../models/post.model")
 
 // post CRUD methods
 
--- Mongo/src/controllers/tag.controller.js
+++ Mongo/src/controllers/tag.controller.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+const Tag = require("../models/tag.model")
 
 
 // tag CRUD requests
