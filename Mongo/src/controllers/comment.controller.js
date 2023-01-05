e71547d7883125fa6afbfb8ad3ffa817f18bfa73
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2023-01-05 23:58:05 +0530

    routers

--- Mongo/src/controllers/comment.controller.js
+++ Mongo/src/controllers/comment.controller.js
@@ -2,17 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
+const Comment = require("../models/comment.model")
 
-// comment CRUD methods
 
-router.post("/comments", async (req, res) => {
-    try {
-        const comment = await Comment.create(req.body);
-        return res.status(201).send(comment);
-    } catch (err) {
-        return res.status(500).send(err.message)
-    }
-})
+    // comment CRUD methods
+
+    router.post("/comments", async (req, res) => {
+        try {
+            const comment = await Comment.create(req.body);
+            return res.status(201).send(comment);
+        } catch (err) {
+            return res.status(500).send(err.message)
+        }
+    })
 
 router.get("/comments", async (req, res) => {
     try {
--- Mongo/src/controllers/user.controller.js
+++ Mongo/src/controllers/user.controller.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const User = require("../models/user.model")
+
 
 // user CRUD methods
 router.post("/mocks", async (req, res) => {
