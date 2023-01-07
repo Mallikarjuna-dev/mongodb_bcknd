810fb252496db994c8b67bc62e08cc150123204f
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2023-01-07 19:24:30 +0530

    mongo connection

--- Mongo/src/configs/db.js
+++ Mongo/src/configs/db.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-
 //step 1: connect to mongoDB
 mongoose.set("strictQuery", false);
 
 const connect = () => {
     return mongoose.connect("mongodb+srv://mallikarjuna27:Ajju_2748@cluster0.orelrvq.mongodb.net/project?retryWrites=true&w=majority")
 }
-
 
 module.exports = connect;
