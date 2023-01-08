acd2e8eaf7949c8cdd3ac7be9c13b7582acbc515
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2023-01-08 22:04:13 +0530

    model

---
+++ pagination/src/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    gender: { type: String, required: true },
+    ip_address: { type: String, required: false }
+},{
+    versionkey:false,
+    time
+})
