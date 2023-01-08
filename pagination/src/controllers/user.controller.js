6adcb92ccc818dd4574ad1712372dba14b42c51b
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2023-01-09 01:04:12 +0530

    Paginate

--- pagination/src/controllers/user.controller.js
+++ pagination/src/controllers/user.controller.js
@@ -10,8 +10,11 @@ router.get("", async (req, res) => {
         const size = req.query.size || 15;
 
         const query = { gender: "Female" };
+
         const users = await User.find(query).skip((page - 1) * size).limit(size).lean().exec();
+
         const total = Math.ceil(await User.find(query).count() / size);
+
         return res.send({ users, total })
     } catch (err) {
         return res.status(500).send({ message: err.message });
