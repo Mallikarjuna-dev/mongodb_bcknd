f9ae81e31bca585785302677e515feb30ed65b86
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2023-01-12 21:35:46 +0530

    validations for ID

--- pagination/src/controllers/user.controller.js
+++ pagination/src/controllers/user.controller.js
@@ -6,22 +6,24 @@ const User = require("../models/user.model")
 
 const router = express.Router();
 
-router.post("", body("id").notEmpty().isNumeric(), async (req, res) => {
-    // console.log(body("id"))
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+router.post("",
+    body("id").isNumeric(),
+    body("first_name").isLength({ min: 5, max: 50 }),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).send({ errors: errors.array() });
+            }
+
+            // const user = await User.create(req.body);
+
+            return res.send("user");
+
+        } catch (err) {
+            return res.status(500).send({ message: err.message })
         }
-
-        // const user = await User.create(req.body);
-
-        return res.send("user");
-
-    } catch (err) {
-        return res.status(500).send({ message: err.message })
-    }
-})
+    })
 
 router.get("", async (req, res) => {
     try {
@@ -30,9 +32,10 @@ router.get("", async (req, res) => {
 
         const query = { gender: "Female" };
 
-        const users = await User.find(query).skip((page - 1) * size).limit(size).lean().exec();
+        const users = await User.find().lean().exec();
+        //skip((page - 1) * size).limit(size).
 
-        const total = Math.ceil(await User.find(query).count() / size);
+        const total = Math.ceil(await User.find().count() / size);
 
         return res.send({ users, total })
 
