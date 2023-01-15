bf443bbaa46798b5beb812378e902496b576e0e1
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2023-01-16 04:22:07 +0530

    val

--- pagination/src/models/user.model.js
+++ pagination/src/models/user.model.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    gender: { type: String, required: true },
-    ip_address: [{ type: String, required: false }],
-}, {
-    versionKey: false,
-    timestamps: true,
-})
+const userSchema = new mongoose.Schema(
+    {
+        id: { type: Number, required: true },
+        first_name: { type: String, required: true },
+        last_name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        gender: { type: String, required: true },
+        ip_address: [{ type: String, required: false }],
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    })
 
 module.exports = mongoose.model("user", userSchema);
